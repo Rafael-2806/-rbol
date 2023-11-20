878fc58b325ec89c8c821ec6e27e54f74740b74d
Rafael-2806 <113630636+Rafael-2806@users.noreply.github.com>
2023-11-20 18:46:06 +0100

    update

--- 16.py
+++ 16.py
@@ -162,3 +162,84 @@ class Arbol(object):
             if filtro(arbol.info):
                 print(arbol.info)
             Arbol.filtrar(arbol.der,filtro)      
+
+class Pokemon:
+    
+    def __init__(self,nombre, numero=0,tipos=[], debilidad=""):
+        self.nombre=nombre
+        self.numero=numero
+        self.tipos=tipos
+        self.debilidad=debilidad
+    
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Numero: {self.numero}, Tipos: {self.tipos}, debilidad: {self.debilidad}"
+        
+    def gana(poke1,poke2): #Si tipo de poke 1 es debilidad de poke 2 gana
+        for i in poke1.tipos:
+            if i == poke2.debilidad:
+                return True #si algun tipo es la debilidad de poke 2
+        return False #si el tipo no esta en la debilidad de poke 2
+    
+
+pokemons_info = [
+    {"nombre": "Bulbasaur", "numero": 1, "tipos": ["Planta", "Veneno"], "debilidad": "Psíquico"},
+    {"nombre": "Ivysaur", "numero": 2, "tipos": ["Planta", "Veneno"], "debilidad": "Psíquico"},
+    {"nombre": "Venusaur", "numero": 3, "tipos": ["Planta", "Veneno"], "debilidad": "Psíquico"},
+    {"nombre": "Charmander", "numero": 4, "tipos": ["Fuego"], "debilidad": "Agua"},
+    {"nombre": "Charmeleon", "numero": 5, "tipos": ["Fuego"], "debilidad": "Agua"},
+    {"nombre": "Charizard", "numero": 6, "tipos": ["Fuego", "Volador"], "debilidad": "Agua"},
+    {"nombre": "Squirtle", "numero": 7, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Wartortle", "numero": 8, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Blastoise", "numero": 9, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Pikachu", "numero": 25, "tipos": ["Eléctrico"], "debilidad": "Tierra"},
+    {"nombre": "Raichu", "numero": 26, "tipos": ["Eléctrico"], "debilidad": "Tierra"},
+    {"nombre": "Jigglypuff", "numero": 39, "tipos": ["Normal", "Hada"], "debilidad": "Acero"},
+    {"nombre": "Wigglytuff", "numero": 40, "tipos": ["Normal", "Hada"], "debilidad": "Acero"},
+    {"nombre": "Psyduck", "numero": 54, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Golduck", "numero": 55, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Machop", "numero": 66, "tipos": ["Lucha"], "debilidad": "Psíquico"},
+    {"nombre": "Machoke", "numero": 67, "tipos": ["Lucha"], "debilidad": "Psíquico"},
+    {"nombre": "Machamp", "numero": 68, "tipos": ["Lucha"], "debilidad": "Psíquico"},
+    {"nombre": "Gyarados", "numero": 130, "tipos": ["Agua", "Volador"], "debilidad": "Eléctrico"},
+    {"nombre": "Vaporeon", "numero": 134, "tipos": ["Agua"], "debilidad": "Planta"},
+    {"nombre": "Jolteon", "numero": 135, "tipos": ["Eléctrico"], "debilidad": "Tierra"},
+    {"nombre": "Flareon", "numero": 136, "tipos": ["Fuego"], "debilidad": "Agua"},
+    {"nombre": "Snorlax", "numero": 143, "tipos": ["Normal"], "debilidad": "Lucha"},
+    {"nombre": "Dragonite", "numero": 149, "tipos": ["Dragón", "Volador"], "debilidad": "Hielo"},
+    {"nombre": "Mewtwo", "numero": 150, "tipos": ["Psíquico"], "debilidad": "Siniestro"},
+    {"nombre": "Mew", "numero": 151, "tipos": ["Psíquico"], "debilidad": "Siniestro"},
+    {"nombre": "Tyranitar", "numero": 248, "tipos": ["Roca", "Siniestro"], "debilidad": "Lucha"},
+    {"nombre": "Blaziken", "numero": 257, "tipos": ["Fuego", "Lucha"], "debilidad": "Agua"},
+    {"nombre": "Gardevoir", "numero": 282, "tipos": ["Psíquico", "Hada"], "debilidad": "Acero"},
+]
+    
+poke_lista=[]
+    
+for p in pokemons_info:
+    poke_lista.append(Pokemon(p["nombre"],p["numero"],p["tipos"],p["debilidad"]))
+    
+#crea arbol por oden de nombre, tipo y numero
+# comparadores por nombre y por color sable -1 si es mas pequeño 0 igual 1 mayor. lambda hace una funcion pero si poner nombre
+comparaNombre=lambda x,y: -1 if x.nombre<y.nombre else 0 if x.nombre==y.nombre else 1
+comparaNumero=lambda x,y: -1 if x.numero<y.numero else 0 if x.numero==y.numero else 1
+comparaTipo=lambda x,y: -1 if x.tipos[0]<y.tipos[0] else 0 if x.tipos[0]==y.tipos[0] else 1  #sepone[0] por que es un diccionario
+
+indice_nombre= Arbol.generar(poke_lista,comparaNombre)
+indice_numero= Arbol.generar(poke_lista,comparaNumero)
+indice_tipo= Arbol.generar(poke_lista,comparaTipo)
+    
+Arbol.pintar(indice_nombre)
+#Buscar por nombre
+nombre=input("nombre del pokemon ")
+arbol_pokemon= Arbol.buscar(indice_nombre,Pokemon(nombre),comparaNombre)
+print(arbol_pokemon)
+
+if arbol_pokemon != None:
+    pokemon=arbol_pokemon.info
+    print(pokemon, "gana a: ")
+    #me va a sacar todos los que gana el que busco
+    Arbol.filtrar(indice_nombre, lambda x:Pokemon.gana(pokemon, x))
+#busqueda por proximidad
+nombre_filtrar=input("primeras leetras del nombre de pokemon")
+Arbol.filtrar(indice_nombre, lambda x: x.nombre.startswith(nombre_filtrar)) # si el nombre del pokemon contiene las primeras letras 
+
